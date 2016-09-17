dedcadfd872ee5cc3754f2777511e3209ec804e6
Naoki_Fujisawa <naoki.fujisawa1@gmail.com>
2016-09-18 03:41:46 +0900

    Add template

---
+++ docs/js/get_spreadsheets.js
@@ -0,0 +1,4 @@
+$.getJSON('https://script.googleusercontent.com/macros/echo?user_content_key=sBt5XWwNDq7pfSfuKEYTXXDaEU3v4LnftV2O7s4dkNuL98PlAdHu4Qr6yBDMaPDN52wyaxScTMudUAT_coy9hrJ56-AhTuj1m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnAxYlnJQR90_AcvmOuq7q-Yy6KWsBzaQalCKYRtXCRd0_1SLFb_ylEundPWNUyxT9g&lib=MQRWKTQkXdVmumSMCx91dOu0xCCpi663s',
+		function(json) {
+		/*optional stuff to do after success */
+});
